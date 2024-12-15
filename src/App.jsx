63dac615dd4ecfa87d5e80000d44207dbe3386cf
Maria <beadleshero@gmail.com>
2024-12-15 14:10:42 -0400

    add: application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Navbar } from './components/Navbar';
+import { Home, Contact, About, Projects  } from './pages'
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </div>
+    <main className='bg-slate-300/20'>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/projects' element={<Projects/>}/>
+                <Route path='/contac' element={<Contact/>}/>
+            </Routes>
+        </Router>
+    </main>
   );
 };
 
---
+++ src/pages/Pojects.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pojects = () => {
+  return (
+    <div>Pojects</div>
+  )
+}
+
+export default Pojects
