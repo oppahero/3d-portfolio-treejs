8771ef98f6c6486e6f87ef01677847bd65365a42
Maria <beadleshero@gmail.com>
2024-12-15 21:40:57 -0400

    add: Bird animation

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,13 +1,44 @@
-import React from "react";
-import { useGLTF } from "@react-three/drei";
-
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useRef, useEffect, act } from "react";
 import birdScene from "../assets/3d/bird.glb";
+import { useFrame } from "@react-three/fiber";
 
 const Bird = () => {
   const { scene, animations } = useGLTF(birdScene);
+  const birdRef = useRef();
+  const { actions } = useAnimations(animations, birdRef);
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, []);
+
+  // Moverlo por la isla
+  useFrame(({ clock, camera })=> {
+    // Update y position to simulate the flight moving in a sin wave
+    // Se movera mientras vaya pasando el tiempo
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2 ;
+
+    // Para moverlo alrededor de la isla (x)
+    // Si el pajaro se ha salido de la camara se quiere que de la vuelta 180°
+    if(birdRef.current.position.x > camera.position.x + 10){
+      birdRef.current.rotation.y = Math.PI
+    }else if(birdRef.current.position.x < camera.position.x - 10){
+      // si no ha llegado al final
+      birdRef.current.rotation.y = 0
+    }
+
+    if(birdRef.current.rotation.y === 0){
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    }else{
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+
+  })
 
   return (
-    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh ref={birdRef} position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
       <primitive object={scene} />
     </mesh>
   );
